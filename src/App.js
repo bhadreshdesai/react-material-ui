98d83a4e7ec4c4cc1215d003fe6f30e551e8b344
Bhadresh Desai <16066580+bhadreshdesai@users.noreply.github.com>
2019-02-10 18:20:20 +0000

    3. Add form with to create exercise
    Dummy for which lets you type in the exercise name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
+import { Button, TextField, Typography } from "@material-ui/core";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { exerciseName: "" };
+  }
+
+  handleChange = ({ target: { name, value } }) =>
+    this.setState({
+      [name]: value
+    });
+
   render() {
     return (
-      <Typography variant="h4" align="center" gutterBottom={true}>
-        Exercises
-      </Typography>
+      <div>
+        <Typography variant="h4" align="center" gutterBottom={true}>
+          Exercises
+        </Typography>
+        <form>
+          <TextField
+            name="exerciseName"
+            label="Exercise Name"
+            value={this.state.exerciseName}
+            onChange={this.handleChange}
+          />
+          <Button type="submit" color="primary" variant="raised">
+            Create
+          </Button>
+        </form>
+      </div>
     );
   }
 }
